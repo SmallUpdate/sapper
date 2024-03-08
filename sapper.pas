bb7cb86884ddc3433956852739d5795da0116a8b
Игорь <112966801+SmallUpdate@users.noreply.github.com>
2024-03-09 00:03:09 +0300

    Add files via upload

---
+++ sapper.pas
@@ -0,0 +1,186 @@
+﻿program sapper;
+
+uses
+  graphABC, ABCObjects;
+
+const
+  windowWidth = 1280;
+  windowHeight = 720;
+  fieldSize = 7; // fieldWidth, fieldHeight
+  countOfMines = fieldSize * fieldSize div 10; // 10%
+  cellSize = windowHeight div (fieldSize + 2); // 60: 1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30
+                                              // 100: 1, 2, 4, 5, 10, 20, 50
+
+var
+  field: array [1..fieldSize] of array [1..fieldSize] of byte;
+  isOpened: array [1..fieldSize] of array [1..fieldSize] of boolean;
+  isFlag: array [1..fieldSize] of array [1..fieldSize] of boolean;
+  firstTurn := true;
+  placeOfMines: array [1..fieldSize * fieldSize] of integer;
+  
+  //prevX, prevY: integer;
+
+procedure setWindow;
+begin
+  SetWindowSize(windowWidth, windowHeight);
+  CenterWindow;
+  SetWindowTitle('Sapper - ultra delux');
+  SetWindowIsFixedSize(true);
+  SetFontName('Consolas');
+  SetFontSize(cellSize div 10 * 6);
+  SetFontStyle(fsBold);
+  for var y := 1 to fieldSize do
+    for var x := 1 to fieldSize do
+    begin
+      field[x][y] := 0;
+      isOpened[x][y] := false;
+      isFlag[x][y] := false;
+    end;
+  for var i := 1 to fieldSize * fieldSize do
+    placeOfMines[i] := i;
+  
+  //prevX := 1;
+  //prevY := 1;
+end;
+
+procedure generateField;
+begin
+  for var y := 1 to fieldSize do
+    for var x := 1 to fieldSize do
+    begin
+      if (x + y) mod 2 = 0 then
+        SetBrushColor(RGB(162, 209, 73))
+      else
+        SetBrushColor(RGB(170, 215, 81));
+      FillRectangle(x * cellSize, y * cellSize, (x + 1) * cellSize, (y + 1) * cellSize);
+    end;
+end;
+
+procedure setNumbers;
+begin
+  for var y := 1 to fieldSize do
+    for var x := 1 to fieldSize do
+      if field[x][y] = 9 then
+        for var dy := -1 to 1 do
+          for var dx := -1 to 1 do
+            if (x + dx > 0) and (x + dx <= fieldSize) and (y + dy > 0) and (y + dy <= fieldSize) and (field[x + dx][y + dy] <> 9) and ((dx <> 0) or (dy <> 0)) then // последнее условие не обязательно
+                field[x + dx][y + dy] += 1;
+end;
+
+procedure generateMines(x, y: integer);
+begin
+  firstTurn := false;
+  var z := 0;
+  for var dy := -1 to 1 do
+    for var dx := -1 to 1 do
+      if (x + dx > 0) and (x + dx <= fieldSize) and (y + dy > 0) and (y + dy <= fieldSize) then
+      begin
+        Swap(placeOfMines[(x - 1 + dx) * fieldSize + y + dy], placeOfMines[fieldSize * fieldSize - z]);
+        z += 1;
+      end;
+  for var i := 1 to countOfMines do
+  begin
+    var rnd := Random(1, (fieldSize * fieldSize - i - z));
+    field[(placeOfMines[rnd] - 1) div fieldSize + 1][(placeOfMines[rnd] - 1) mod fieldSize + 1] := 9;
+    Swap(placeOfMines[rnd], placeOfMines[fieldSize * fieldSize - i - z]);
+  end;
+  setNumbers;            
+end;
+
+procedure openCell(x, y: integer);
+begin
+  if (x > 0) and (x <= fieldSize) and (y > 0) and (y <= fieldSize) and not isOpened[x][y] and not isFlag[x][y] then
+  begin
+    isOpened[x][y] := true;
+    if (x + y) mod 2 = 0 then
+       SetBrushColor(RGB(215, 184, 153))
+    else
+      SetBrushColor(RGB(229, 194, 159));
+    FillRectangle(x * cellSize, y * cellSize, (x + 1) * cellSize, (y + 1) * cellSize);
+    if firstTurn then
+    GenerateMines(x, y);
+    if field[x][y] <> 0 then
+      TextOut(x * cellSize + 17, y * cellSize + 2, field[x][y].ToString)
+    else
+      for var dy := -1 to 1 do
+        for var dx := -1 to 1 do
+          if (dx <> 0) or (dy <> 0) then
+            openCell(x + dx, y + dy);
+  end;
+end;
+
+procedure mouseUp(x, y, mb: integer);
+begin
+  x := x div cellSize;
+  y := y div cellSize;
+  SetWindowTitle('x: ' + x + ' y: ' + y);
+  if mb = 1 then
+    openCell(x, y)
+  else // set flag
+  begin
+    if not isOpened[x][y] then
+    begin
+      if isFlag[x][y] then
+      begin
+        if (x + y) mod 2 = 0 then
+          SetBrushColor(RGB(162, 209, 73))
+        else
+          SetBrushColor(RGB(170, 215, 81));
+        FillRectangle(x * cellSize, y * cellSize, (x + 1) * cellSize, (y + 1) * cellSize);
+      end
+      else
+      begin
+        if (x + y) mod 2 = 0 then
+           SetBrushColor(RGB(209, 73, 73))
+        else
+          SetBrushColor(RGB(215, 81, 81));
+        FillRectangle(x * cellSize, y * cellSize, (x + 1) * cellSize, (y + 1) * cellSize);
+      end;
+      isFlag[x][y] := not isFlag[x][y];
+    end;
+  end;
+end;
+
+{procedure mouseMove(x, y, mb: integer);
+begin
+  x := x div cellSize;
+  y := y div cellSize;
+  if (x > 0) and (x <= fieldSize) and (y > 0) and (y <= fieldSize) and not isOpened[x][y] and not isFlag[x][y] then
+  begin
+    if (x + y) mod 2 = 0 then
+       SetBrushColor(RGB(185, 221, 119))
+    else
+      SetBrushColor(RGB(191, 225, 125));
+    FillRectangle(x * cellSize, y * cellSize, (x + 1) * cellSize, (y + 1) * cellSize);
+    
+    if ((prevX <> x) or (prevY <> y)) and not isOpened[prevX][prevY] and not isFlag[prevX][prevY] then
+    begin
+      if (prevX + prevY) mod 2 = 0 then
+        SetBrushColor(RGB(162, 209, 73))
+      else
+        SetBrushColor(RGB(170, 215, 81));
+    FillRectangle(prevX * cellSize, prevY * cellSize, (prevX + 1) * cellSize, (prevY + 1) * cellSize);
+    end;
+    prevX := x;
+    prevY := y;
+  end
+  else
+    if ((prevX <> x) or (prevY <> y)) and not isOpened[prevX][prevY] and not isFlag[prevX][prevY] then
+    begin
+      if (prevX + prevY) mod 2 = 0 then
+        SetBrushColor(RGB(162, 209, 73))
+      else
+        SetBrushColor(RGB(170, 215, 81));
+    FillRectangle(prevX * cellSize, prevY * cellSize, (prevX + 1) * cellSize, (prevY + 1) * cellSize);
+    end;
+end;}
+
+begin
+  setWindow;
+  generateField;
+  {for var i := 1 to fieldSize * fieldSize do
+    mouseUp(((i - 1) div fieldSize + 1) * cellSize, ((i - 1) mod fieldSize + 1) * cellSize, 1);}
+  OnMouseUp := mouseUp;
+//OnMouseMove := mouseMove;
+//SaveWindow('window.png');
+end.
---
+++ sapperOrigins.pas
@@ -0,0 +1,171 @@
+﻿program sapper;
+
+uses
+  graphABC;
+
+const
+  windowWidth = 1200;
+  windowHeight = 660;
+  fieldWidth = 20;
+  fieldHeight = 10; 
+  countOfMines = fieldWidth * fieldHeight div 10; // 100% : 10 = 10%
+  cellSize = 60;
+
+var
+  field: array [1..fieldWidth] of array [1..fieldHeight] of byte;
+  isOpened: array [1..fieldWidth] of array [1..fieldHeight] of boolean;
+  isFlag: array [1..fieldWidth] of array [1..fieldHeight] of boolean;
+  firstTurn := true;
+  isGameOver := false;
+  placeOfMines: array [1..fieldWidth * fieldHeight] of integer;
+  openedCells := 0;
+  
+  prevX, prevY: integer;
+
+procedure FillR(x, y: integer; c1, c2: Color);
+begin
+  if (x + y) mod 2 = 0 then
+    SetBrushColor(c1)
+  else
+    SetBrushColor(c2);
+    FillRectangle((x - 1) * cellSize, y * cellSize, x * cellSize, (y + 1) * cellSize);
+end;
+
+procedure generateField;
+begin
+  for var y := 1 to fieldHeight do
+    for var x := 1 to fieldWidth do
+      FillR(x, y, RGB(162, 209, 73), RGB(170, 215, 81));
+end;
+
+procedure startGame;
+begin
+  for var y := 1 to fieldHeight do
+    for var x := 1 to fieldWidth do
+    begin
+      field[x][y] := 0;
+      isOpened[x][y] := false;
+      isFlag[x][y] := false;
+    end;
+  for var i := 1 to fieldWidth * fieldHeight do
+    placeOfMines[i] := i;
+  
+  prevX := 1;
+  prevY := 1;
+  
+  generateField;
+end;
+
+procedure setWindow;
+begin
+  SetWindowSize(windowWidth, windowHeight);
+  CenterWindow;
+  SetWindowTitle('Sapper - by Pesniakov Igor');
+  SetWindowIsFixedSize(true);
+  SetFontName('Consolas');
+  ClearWindow(RGB(74, 117, 44));
+  SetFontSize(cellSize div 10 * 6);
+  SetFontStyle(fsBold);
+  startGame;
+end;
+
+procedure setNumbers;
+begin
+  for var y := 1 to fieldHeight do
+    for var x := 1 to fieldWidth do
+      if field[x][y] = 9 then
+        for var dy := -1 to 1 do
+          for var dx := -1 to 1 do
+            if (x + dx > 0) and (x + dx <= fieldWidth) and (y + dy > 0) and (y + dy <= fieldHeight) and (field[x + dx][y + dy] <> 9) and ((dx <> 0) or (dy <> 0)) then // последнее условие не обязательно
+                field[x + dx][y + dy] += 1;
+end;
+
+procedure generateMines(x, y: integer);
+begin
+  firstTurn := false;
+  var z := 0;
+  for var dy := -1 to 1 do
+    for var dx := -1 to 1 do
+      if (x + dx > 0) and (x + dx <= fieldWidth) and (y + dy > 0) and (y + dy <= fieldHeight) then
+      begin
+        Swap(placeOfMines[(x - 1 + dx) * fieldHeight + y + dy], placeOfMines[fieldWidth * fieldHeight - z]);
+        z += 1;
+      end;
+  for var i := 1 to countOfMines do
+  begin
+    var rnd := Random(1, (fieldWidth * fieldHeight - i - z));
+    field[(placeOfMines[rnd] - 1) div fieldHeight + 1][(placeOfMines[rnd] - 1) mod fieldHeight + 1] := 9;
+    Swap(placeOfMines[rnd], placeOfMines[fieldWidth * fieldHeight - i - z]);
+  end;
+  setNumbers;            
+end;
+
+procedure openCell(x, y: integer);
+begin
+  if (x > 0) and (x <= fieldWidth) and (y > 0) and (y <= fieldHeight) and not isOpened[x][y] and not isFlag[x][y] then
+  begin
+    isOpened[x][y] := true;
+    openedCells += 1;
+    FillR(x, y, RGB(215, 184, 153), RGB(229, 194, 159));
+    if firstTurn then
+    GenerateMines(x, y);
+    if field[x][y] <> 0 then
+    begin
+      SetFontColor(RGB(0 + (field[x][y] - 1) * 31, 127, 255 - (field[x][y] - 1) * 31));
+      TextOut(x * cellSize - 43, y * cellSize + 2, field[x][y].ToString);
+      if (field[x][y] = 9) or (openedCells = fieldWidth * fieldHeight - countOfMines) then
+        isGameOver := true;
+    end
+    else
+      for var dy := -1 to 1 do
+        for var dx := -1 to 1 do
+          if (dx <> 0) or (dy <> 0) then
+            openCell(x + dx, y + dy);
+  end;
+end;
+
+procedure mouseUp(x, y, mb: integer);
+begin
+  if not isGameOver then
+  begin
+    x := x div cellSize + 1;
+    y := y div cellSize;
+    if mb = 1 then
+      openCell(x, y)
+    else // set flag
+    begin
+      if (y > 0) and not isOpened[x][y] and not firstTurn then
+      begin
+        if isFlag[x][y] then
+          FillR(x, y, RGB(162, 209, 73), RGB(170, 215, 81))
+        else
+          FillR(x, y, RGB(209, 73, 73), RGB(215, 81, 81));
+        isFlag[x][y] := not isFlag[x][y];
+      end;
+    end;
+  end;
+end;
+
+procedure mouseMove(x, y, mb: integer);
+begin
+  x := x div cellSize + 1;
+  y := y div cellSize;
+  if (x > 0) and (x <= fieldWidth) and (y > 0) and (y <= fieldHeight) and not isOpened[x][y] and not isFlag[x][y] then
+  begin
+    FillR(x, y, RGB(185, 221, 119), RGB(191, 225, 125));
+    
+    if ((prevX <> x) or (prevY <> y)) and not isOpened[prevX][prevY] and not isFlag[prevX][prevY] then
+      FillR(prevX, prevY, RGB(162, 209, 73), RGB(170, 215, 81));
+    prevX := x;
+    prevY := y;
+  end
+  else
+    if ((prevX <> x) or (prevY <> y)) and not isOpened[prevX][prevY] and not isFlag[prevX][prevY] then
+      FillR(prevX, prevY, RGB(162, 209, 73), RGB(170, 215, 81));
+end;
+
+begin
+  setWindow;
+  OnMouseUp := mouseUp;
+  OnMouseMove := mouseMove;
+end.
